da51331e5a8392e46804afac4e186585d2a32141
Fernando <u201617247@upc.edu.pe>
2020-06-09 15:39:16 -0500

    ayuda

---
+++ BonosCalculadora/Models/Calculadora.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BonosCalculadora.Models
+{
+    public class Calculadora
+    {
+        string valornominal { get; set; }
+    }
+}
